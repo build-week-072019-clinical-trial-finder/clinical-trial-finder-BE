68bf3e022dd241b40519f71c3196babfaaddb243
George Adams <gadams@Georges-MacBook-Air.local>
2019-07-29 18:33:55 -0500

    genesis block for the basic get

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,7 @@
 const knex = require("knex");
 
-const knexConfig = require("../knexfile");
+const config = require("../knexfile");
 
-module.exports = knex(knexConfig.development);
+const dbEnv = process.env.DB_ENV || "development";
+
+module.exports = knex(config[dbEnv]);
--- server.js
+++ server.js
@@ -15,7 +15,9 @@ server.use("/api", authRouter);
 server.use("/api", usersRouter);
 
 server.get("/", (req, res) => {
-  res.send("It's alive!");
+  res.send(
+    "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks"
+  );
 });
 
 module.exports = server;
