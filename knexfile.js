14ebf0b2447d6793f544385beabd8d5336d0c92e
George Adams <gadams@Georges-MacBook-Air.local>
2019-07-29 18:20:13 -0500

    adds pg support

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,15 @@ module.exports = {
     seeds: {
       directory: "./database/seeds"
     }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
   }
 };
