6a74b6a5e01cb474c31a3e44a7c64f90135f6cf9
George Adams <gadams@Georges-MacBook-Air.local>
2019-07-29 20:04:48 -0500

    try to fix the id

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -26,11 +26,11 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = generateToken(user); // <<<<<<<<<<<<<<<<<<<<
+        const token = generateToken(user);
 
         res.status(200).json({
           message: `Welcome ${user.username}!`,
-          token /// <<<<<<<<<<<<<<<<<<<<<<<<<<<<
+          token
         });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
--- users/users-model.js
+++ users/users-model.js
@@ -16,7 +16,7 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  const [id] = await db("users").insert(user);
+  const [id] = await db("users").insert(user, "id");
 
   return findById(id);
 }
